adfaf38c2dd3ec7ce06868ccea27a730376711eb
André Donadon <andreltdonadon@gmail.com>
2023-07-21 17:49:26 -0300

    Last update

--- content-manager/src/main/java/com/donadoncore/distrocontrast/contentmanager/api/application/service/user/UserService.java
+++ content-manager/src/main/java/com/donadoncore/distrocontrast/contentmanager/api/application/service/user/UserService.java
@@ -1,10 +1,10 @@
 package com.donadoncore.distrocontrast.contentmanager.api.application.service.user;
 
 import com.donadoncore.distrocontrast.contentmanager.api.domain.device.DeviceFormRequest;
-import com.donadoncore.distrocontrast.contentmanager.api.domain.device.DeviceResponse;
+import com.donadoncore.distrocontrast.contentmanager.api.domain.user.User;
 
 public interface UserService {
 
-    DeviceResponse insertDevice(String userId, DeviceFormRequest formRequest);
+    User insertDevice(String userId, DeviceFormRequest formRequest);
 
 }
